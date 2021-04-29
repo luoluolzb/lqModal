da01e9987f041baf41a5d52d8097fff9811b1ea8
luoluolzb <luoluolzb@163.com>
2021-04-29 18:36:56 +0800

    support boostrap5.0

--- lqModal.js
+++ lqModal.js
@@ -2,8 +2,8 @@
  * 简单模态框：信息框、操作确认框、信息输入框
  * 此功能建立在jquery和bootstrap的模态框插件基础上
  * @author  luoluolzb
- * @version 2.0
- * @time    2019/5/25
+ * @version 3.0
+ * @time    2021/4/29
  */
 var lqModal = {};
 
@@ -15,56 +15,56 @@ $(function(){
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title"></h5>
-					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+					<h5 class="modal-title">提示消息</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 				</div>
 				<div class="modal-body"></div>
 				<div class="modal-footer">
-					<button type="button" class="okBtn btn btn-primary" data-dismiss="modal">确定</button>
+					<button type="button" class="okBtn btn btn-primary" data-bs-dismiss="modal">确定</button>
 				</div>
 			</div>
 		</div>
 	</div>
-	
+
 	<!-- dialog Modal -->
 	<div class="modal fade" id="lqModal-dialog-modal" tabindex="-1">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title"></h5>
-					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+					<h5 class="modal-title">操作提示</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 				</div>
 				<div class="modal-body"></div>
 				<div class="modal-footer">
-					<button type="button" class="okBtn btn btn-primary" data-dismiss="modal">确定</button>
-					<button type="button" class="cancelBtn btn btn-secondary" data-dismiss="modal">取消</button>
+					<button type="button" class="okBtn btn btn-primary" data-bs-dismiss="modal">确定</button>
+					<button type="button" class="cancelBtn btn btn-secondary" data-bs-dismiss="modal">取消</button>
 				</div>
 			</div>
 		</div>
 	</div>
-	
+
 	<!-- input Modal -->
 	<div class="modal fade" id="lqModal-input-modal" tabindex="-1">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title"></h5>
-					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+					<h5 class="modal-title">输入信息</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 				</div>
 				<div class="modal-body">
 					<label></label>
 					<input type="text" class="form-control" value="" />
 				</div>
 				<div class="modal-footer">
-					<button type="button" class="okBtn btn btn-primary" data-dismiss="modal">确定</button>
-					<button type="button" class="cancelBtn btn btn-secondary" data-dismiss="modal">取消</button>
+					<button type="button" class="okBtn btn btn-primary" data-bs-dismiss="modal">确定</button>
+					<button type="button" class="cancelBtn btn btn-secondary" data-bs-dismiss="modal">取消</button>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 	`));
-	
+
 	/**
 	 * 模态框的jQuery对象表
 	 * @type {Object}
@@ -74,7 +74,7 @@ $(function(){
 		$dialog:  $('#lqModal-dialog-modal'),
 		$input:   $('#lqModal-input-modal'),
 	};
-	
+
 	// 快速访问
 	var $message = lqModal.modals.$message;
 	var $dialog  = lqModal.modals.$dialog;
@@ -85,22 +85,20 @@ $(function(){
 	 * @param  {Object} options 参数
 	 */
 	lqModal.message = function(options) {
-		// 如果只传入一个字符串则作为消息文本
+		// 如果只传入一个字符串则作为说明文本
 		if (typeof options === 'string') {
 			options = {
 				text: options,
 			};
 		}
 		options = $.extend({
-			title: '提示消息',     // 消息框标题
-			text : '',            // 消息内容
-			ok   : undefined,     // 点击'确定'按钮的回调函数
+			text: '',            // 说明文本
+			ok: undefined,       // 点击'确定'按钮的回调函数
 		}, options || {});
 
-		$message.find('.modal-title').html(options.title);
 		$message.find('.modal-body').html(options.text);
 		$message.find('.okBtn').off('click').on('click', options.ok);
-		$message.modal('show');
+		(new bootstrap.Modal($message[0])).show();
 	};
 
 	/**
@@ -109,17 +107,15 @@ $(function(){
 	 */
 	lqModal.dialog = function(options) {
 		options = $.extend({
-			title : '操作提示',    // 对话框标题
-			text  : '',           // 说明文本
-			ok    : undefined,    // 点击'确定'按钮的回调函数
-			cancel: undefined,    // 点击'取消'按钮的回调函数
+			text: '',            // 说明文本
+			ok: undefined,       // 点击'确定'按钮的回调函数
+			cancel: undefined,   // 点击'取消'按钮的回调函数
 		}, options || {});
 
-		$dialog.find('.modal-title').html(options.title);
 		$dialog.find('.modal-body').html(options.text);
 		$dialog.find('.okBtn').off('click').on('click', options.ok);
 		$dialog.find('.cancelBtn').off('click').on('click', options.cancel);
-		$dialog.modal('show');
+		(new bootstrap.Modal($dialog[0])).show();
 	};
 
 	/**
@@ -128,23 +124,21 @@ $(function(){
 	 */
 	lqModal.input = function(options) {
 		options = $.extend({
-			title : '输入信息',    // 输入框标题
-			text  : '',           // 说明文本
-			ok    : undefined,    // 点击'确定'按钮的回调函数
-			cancel: undefined,    // 点击'取消'按钮的回调函数
-			input : {             // 输入模态框中input元素属性
+			text: '',            // 说明文本
+			ok: undefined,       // 点击'确定'按钮的回调函数
+			cancel: undefined,   // 点击'取消'按钮的回调函数
+			input: {             // 输入模态框中input元素属性
 				type: 'text',
 				value: '',
 				placeholder: '',
 			},
 		}, options || {});
 
-		$input.find('.modal-title').html(options.title);
 		$input.find('label').html(options.text);
 		$input.find('input').attr(options.input).val(options.input.value);
 		$input.find('.okBtn').off('click').on('click', options.ok);
 		$input.find('.cancelBtn').off('click').on('click', options.cancel);
-		$input.modal('show');
+		(new bootstrap.Modal($input[0])).show();
 	};
 
 	/**
